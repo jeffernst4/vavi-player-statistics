287d30c5833b650310594d77097ad1b2b688d727
Jeff Ernst <jrernst@calpoly.edu>
2017-10-30 21:39:39 -0700

    renamed leagues script created players script

--- scripts/leagues.R
+++ scripts/leagues.R
@@ -8,23 +8,24 @@ library(rvest)
 # IDK WHAT TO CALL THIS -----
 
 # Read leagues html page
-leagues <-
+leaguesHTML <-
   read_html(
     "http://govavi.leagueapps.com/leagues/soccer?state=COMPLETED&locationId=&seasonId=&days=&levelId="
   )
 
 # Extract league ids
-leagueIds <- leagues %>%
+leagueIds <- leaguesHTML %>%
   html_nodes(xpath = '//*[@id="content"]/div/div/div[3]/div[3]') %>%
   html_nodes("li") %>%
   html_attr("id")
+leagueIds <- gsub("baseevent-", "", leagueIds)
 
 # Extract league names
 leagueNames <- NULL
 for (i in 1:length(leagueIds)) {
   leagueNames[i] <-
-    leagues %>%
-    html_nodes(xpath = paste("//*[@id='", leagueIds[i], "']/div[1]/div/h2", sep = "")) %>%
+    leaguesHTML %>%
+    html_nodes(xpath = paste("//*[@id='baseevent-", leagueIds[i], "']/div[1]/div/h2", sep = "")) %>%
     html_nodes("a") %>%
     html_text()
 }
@@ -33,8 +34,8 @@ for (i in 1:length(leagueIds)) {
 leagueLinks <- NULL
 for (i in 1:length(leagueIds)) {
   leagueLinks[i] <-
-    leagues %>%
-    html_nodes(xpath = paste("//*[@id='", leagueIds[i], "']/div[1]/div/h2", sep = "")) %>%
+    leaguesHTML %>%
+    html_nodes(xpath = paste("//*[@id='baseevent-", leagueIds[i], "']/div[1]/div/h2", sep = "")) %>%
     html_nodes("a") %>%
     html_attr("href")
 }
@@ -43,12 +44,23 @@ for (i in 1:length(leagueIds)) {
 leagueDates <- NULL
 for (i in 1:length(leagueIds)) {
   leagueDates[i] <-
-    leagues %>%
-    html_nodes(xpath = paste("//*[@id='", leagueIds[i], "']/div[1]/div/dl/dd[2]", sep = "")) %>%
+    leaguesHTML %>%
+    html_nodes(xpath = paste("//*[@id='baseevent-", leagueIds[i], "']/div[1]/div/dl/dd[2]", sep = "")) %>%
     html_text()
 }
-leagueStart <- substr(leagueDates, 10, 19)
-leagueEnd <- substr(leagueDates, 65, 74)
+leagueStart <- as.Date(substr(leagueDates, 10, 19))
+leagueEnd <- as.Date(substr(leagueDates, 65, 74))
+
+# Create data frame
+leagues <-
+  data.frame(
+    Id = leagueIds,
+    Name = leagueNames,
+    Link = leagueLinks,
+    Start = leagueStart,
+    End = leagueEnd,
+    stringsAsFactors = FALSE
+  )
 
 
 
