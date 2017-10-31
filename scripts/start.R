8091b7d5ddeba566c60f858797dcd4ffb8363adc
Jeff Ernst <jrernst@calpoly.edu>
2017-10-30 21:28:58 -0700

    added league dates, links, and names

--- scripts/start.R
+++ scripts/start.R
@@ -5,63 +5,50 @@
 library(rvest)
 
 
-# Read html
-leagues <- read_html("http://govavi.leagueapps.com/leagues/soccer?state=COMPLETED&locationId=&seasonId=&days=&levelId=")
+# IDK WHAT TO CALL THIS -----
 
-leagues %>%
+# Read leagues html page
+leagues <-
+  read_html(
+    "http://govavi.leagueapps.com/leagues/soccer?state=COMPLETED&locationId=&seasonId=&days=&levelId="
+  )
+
+# Extract league ids
+leagueIds <- leagues %>%
   html_nodes(xpath = '//*[@id="content"]/div/div/div[3]/div[3]') %>%
   html_nodes("li") %>%
   html_attr("id")
 
-leagues %>%
-  html_nodes(xpath = '//*[@id="baseevent-99577"]/div[1]/div/h2') %>%
-  html_nodes("a") %>%
-  html_attr("href")
-
-leagues %>%
-  html_nodes(xpath = '//*[@id="baseevent-99577"]/div[1]/div/h2') %>%
-  html_nodes("a") %>%
-  html_text()
-
-
-
-html_nodes(xpath = '//*[@id="a"]') %>%
-  html_attr("value")
-
-leagues %>%
-  html_nodes("a") %>%
-  html_attr("href")
-
-# Identify number of dogs in database
-dogcount <- dogs %>%
-  html_nodes(xpath = "//*[@id='dogsearch']/p[2]/strong") %>%
-  html_text() %>%
-  as.numeric()
-
-# Retreive Dog ID's
-dog_id <- NULL
-for (i in 1:dogcount) {
-  dog_id[i] <-
-    dogs %>%
-    html_nodes(xpath = paste("//*[@id='dogsearch']/div[", i, "]/div/div[1]/div/ul/li[2]/a", sep = "")) %>%
-    html_text() %>%
-    str_sub(start = -4) %>%
-    as.numeric()
+# Extract league names
+leagueNames <- NULL
+for (i in 1:length(leagueIds)) {
+  leagueNames[i] <-
+    leagues %>%
+    html_nodes(xpath = paste("//*[@id='", leagueIds[i], "']/div[1]/div/h2", sep = "")) %>%
+    html_nodes("a") %>%
+    html_text()
 }
 
-# Create Dog Data Frame
-dog_df <- data.frame(ID = dog_id)
-for (i in 1:10) {
-  dog_df[, i + 1] <- NA
+# Extract league links
+leagueLinks <- NULL
+for (i in 1:length(leagueIds)) {
+  leagueLinks[i] <-
+    leagues %>%
+    html_nodes(xpath = paste("//*[@id='", leagueIds[i], "']/div[1]/div/h2", sep = "")) %>%
+    html_nodes("a") %>%
+    html_attr("href")
 }
 
-# Retreive Dog Stats
-for (i in 1:length(dog_id)) {
-  dog <- read_html(paste("http://www.labsandmore.org/dog/?animalidnumber=", dog_id[i], sep = ""))
-  breed <-
-    dog %>%
-    html_nodes(xpath = "//*[@id='details']/table") %>%
-    html_table()
-  breed <- t(breed[[1]])
-  dog_df[dog_df$ID == dog_id[i], 2:11] <- breed[2, -11]
+# Extract league dates
+leagueDates <- NULL
+for (i in 1:length(leagueIds)) {
+  leagueDates[i] <-
+    leagues %>%
+    html_nodes(xpath = paste("//*[@id='", leagueIds[i], "']/div[1]/div/dl/dd[2]", sep = "")) %>%
+    html_text()
 }
+leagueStart <- substr(leagueDates, 10, 19)
+leagueEnd <- substr(leagueDates, 65, 74)
+
+
+
